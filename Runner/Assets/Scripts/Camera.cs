94125f2373433219871f2e089bed667ed9fc693a
ilromali <iliashenkoroman@gmail.com>
2020-08-28 23:29:52 +0300

    Added character and camera animation

---
+++ Runner/Assets/Scripts/Camera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private float animationDuration = 3f;
+
+    private Transform playerLocation;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+    private Vector3 animationOffset = new Vector3(0, 4f, 0);
+
+
+    private float transition;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - playerLocation.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = playerLocation.position + startOffset;
+        moveVector.x = 0;
+        moveVector.y = Mathf.Clamp(moveVector.y, 1, 6);
+
+        if (transition > 1f)
+        {
+            transform.position = moveVector;
+        }
+        else
+        {
+
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / AnimationDuration;
+            transform.LookAt(playerLocation.position + Vector3.up);
+        }
+    }
+
+    
+    public float AnimationDuration { get => animationDuration;}
+}
---
+++ Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 1f;
+    [SerializeField] float gravity = 10f;
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float verticalVelocity;
+
+    private float cameraAnimationDuration;
+
+    void Start()
+    {
+        cameraAnimationDuration = FindObjectOfType<Camera>().AnimationDuration;
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < cameraAnimationDuration)
+        {
+            controller.Move(Vector3.forward * playerSpeed * Time.deltaTime);
+           
+            return;
+
+        }
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity;
+        }
+        
+
+
+        moveVector = Vector3.zero;
+        
+        
+            moveVector.x = Input.GetAxisRaw("Horizontal");
+        
+        //jump here
+        moveVector.y = verticalVelocity;
+        moveVector.z = playerSpeed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
